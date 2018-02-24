98c30bc92988a0dfd88c86944367ebc71dbf70f4
Prakash <prakasharul94@gmail.com>
2018-02-24 15:42:30 +0530

    angularjs + controller + json data

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,15 @@
-var app = angular.module("codelife_btc", ["ngRoute"]);
+var btc = angular.module("codelife_btc", ["ngRoute"]);
+
+
+// news ccontroller
+btc.controller('controller_news', function($scope,$http){
+	$http({
+		method: 'POST', 
+		url: '../assets/data/data.json', 
+		dataType:"json", 
+		contentType:"application/json; charset=utf-8"})
+	.then(function(response){
+		console.log(response);
+		$scope.news = response
+	})
+});
