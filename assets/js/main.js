71ac17978d8014530b38e4bc2a97a356de41dec8
jeeth <jeeth@codelife.ml>
2018-02-25 22:08:50 +0530

    path

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ var btc = angular.module("codelife_btc", ["ngRoute"]);
 // news ccontroller
 btc.controller('controller_news', function($scope,$http){
 
-	$http.get('../assets/data/data.json')
+	$http.get('assets/data/data.json')
 	.then(function(response){
 		console.log(response);
 		$scope.news = response.data;
