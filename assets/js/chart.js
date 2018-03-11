e393582a34f7f62680ab908b69ebe9a0c6eebd33
jeeth <jeeth@codelife.ml>
2018-03-11 15:50:22 +0530

    ui comment section graph data added

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 
-     $.getJSON(
-        'https://api.blockchain.info/charts/market-price?format=json&cors=true',
+    $.getJSON(
+        'https://blockchain.info/charts/market-price?daysAverageString=7&format=json&cors=true',
         function(data) {
-
+            console.log(data);
             Highcharts.chart('container', {
                 chart: {
                     zoomType: 'x'
@@ -53,6 +53,25 @@ $(document).ready(function() {
                     }
                 },
 
+                tooltip: {
+                    formatter: function() {
+                        var date = new Date(this.x*1000);
+                        var dateStr = date.getFullYear() + '/' + ('0' + (date.getMonth() + 1)).slice(-2) + '/' + ('0' + date.getDate()).slice(-2) + ' ' + ('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2);
+                        var tooltip = '<b>' + dateStr + '</b><br/><b>' + this.series.name + '</b>: ';
+
+                        if (this.y === 0.00001) {
+                            tooltip += '<= 0.00001';
+                        } else {
+                            if (this.y % 1 != 0 && this.y <= 10) {
+                                tooltip += Highcharts.numberFormat(this.y, 3, '.', ',');
+                            } else {
+                                tooltip += Highcharts.numberFormat(this.y, 0, '.', ',');
+                            }
+                        }
+
+                        return tooltip;
+                    }
+                },
                 series: [{
                     type: 'area',
                     name: 'Rupee',
@@ -189,5 +208,5 @@ $(document).ready(function() {
 
     //     });
 
-   
+
 });
